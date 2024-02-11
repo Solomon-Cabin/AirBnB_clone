d0db5e9f2cd525afd6b3e4dd1728982ff1e2cc3c
itumeleng <malematjai@gmail.com>
2024-02-10 23:06:40 -0800

    re testing

---
+++ models/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Defines the User class."""
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Represent a User.
+
+    Attributes:
+        email (str): The email of the user.
+        password (str): The password of the user.
+        first_name (str): The first name of the user.
+        last_name (str): The last name of the user.
+    """
+    def __init__(self, *args, **kwargs):
+        """Initialize a new User."""
+        super().__init__(*args, **kwargs)
+        self.email = kwargs.get('email', '')
+        self.password = kwargs.get('password', '')
+        self.first_name = kwargs.get('first_name', '')
+        self.last_name = kwargs.get('last_name', '')
